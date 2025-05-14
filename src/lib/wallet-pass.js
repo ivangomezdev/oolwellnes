a148e746eb3a5a7f0d3a8dd8f8b7f2c7a54c2b6e
ivan <ivansangomez6@gmail.com>
2025-05-14 00:32:57 -0500

    add cahnges

--- src/lib/wallet-pass.js
+++ src/lib/wallet-pass.js
@@ -67,7 +67,7 @@ function createPassJson(ticketId, email, eventName, eventDate) {
         {
           key: 'event',
           label: 'Evento',
-          value: eventName,
+          value: "",
         },
       ],
       secondaryFields: [
