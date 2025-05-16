acf5c42ba1f9d140fa9194bc335f45302e225965
ivan <ivansangomez6@gmail.com>
2025-05-15 20:02:17 -0500

    cambiando partes

--- src/components/QuienesSomos.jsx
+++ src/components/QuienesSomos.jsx
@@ -4,7 +4,7 @@ const QuienesSomos = () => {
   return (
     <div className='quienesSomos__cont'>
       <h1 className='quienesSomos__who'>Nosotros</h1>
-      <h1><span style={{color:"##9F9668"}}>óol</span> proviene de la lengua maya, se traduce como <span style={{color:"#9B956A"}}> "corazón", "alma",  ”esencia".</span> <br /> Es el centro de las emociones y la conciencia. Es una palabra sagrada que representa el estado interior del ser, <span style={{color:"#9B956A"}}>la conexión entre mente, cuerpo y espíritu.</span>
+      <h1><span style={{color:"##9F9668"}}> óol </span> proviene de la lengua maya, se traduce como <span style={{color:"#9B956A"}}> "corazón", "alma",  ”esencia".</span> <br /> Es el centro de las emociones y la conciencia. Es una palabra sagrada que representa el estado interior del ser, <span style={{color:"#9B956A"}}>la conexión entre mente, cuerpo y espíritu.</span>
 </h1><hr />
 <h1>Somos una empresa dedicada a la creación de experiencias expansivas enfocadas al bienestar integral del ser humano. Diseñamos encuentros conscientes donde cuerpo, mente y espíritu se alinean en espacios que <span style={{color:"#9B956A"}}> inspiran, nutren y transforman. </span>
 
