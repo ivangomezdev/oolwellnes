219af750b7fae259a982e6ab1e4b7062f18ee989
ivan <ivansangomez6@gmail.com>
2025-05-18 21:16:59 -0500

    add changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,25 +20,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NLMGN5N4');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
+     
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript
+       <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-WVPP8WYXHC`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLMGN5N4"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WVPP8WYXHC');
+            `,
           }}
         />
         {/* End Google Tag Manager (noscript) */}
